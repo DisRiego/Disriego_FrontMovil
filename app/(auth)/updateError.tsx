f13cba78c090d7540728ebed0f90056318562818
Aleja <u20211195668@usco.edu.co>
2025-03-03 19:51:27 -0500

    Completar perfil

---
+++ app/(auth)/updateError.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import StatusCard from "@/components/StatusCard";
+import { View, StyleSheet, Text } from "react-native";
+import { colors } from "../../config/theme";
+import { typography } from "../../config/typography";
+import Button from "../../components/Button";
+
+interface UpdateSuccessProps {
+    status?: "exito" | "error";
+    message?: string;
+    description?: string;
+    onPress: () => void;
+}
+
+const UpdateSuccess: React.FC<UpdateSuccessProps> = ({
+    status = "error",
+    message = status === "error"
+        ? "Error en la actualización"
+        : "¡Actualización exitosa!",
+    description = status === "error"
+        ? "Hubo un problema al guardar los cambios. Por favor, inténtalo de nuevo."
+        : "Los cambios han sido guardados correctamente.",
+    onPress,
+}) => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.formContainer}>
+                <StatusCard
+                    status={status}
+                    message={message}
+                    description={description}
+                    onPress={onPress}
+                />
+            </View>
+            <View style={styles.footerContainer}>
+                <Button
+                    text={status === "error" ? "Siguiente" : "Reintentar"}
+                    onPress={onPress}
+                />
+            </View>
+        </View>
+    );
+};
+
+export default UpdateSuccess;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "flex-start",
+        backgroundColor: colors.base,
+    },
+    formContainer: {
+        flex: 1,
+        gap: 14,
+        width: "100%",
+        paddingTop: 24,
+        paddingHorizontal: 20,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: colors.base,
+    },
+    footerContainer: {
+        width: "100%",
+        marginTop: 2,
+        paddingTop: 24,
+        paddingBottom: 32,
+        paddingHorizontal: 20,
+        alignItems: "center",
+    },
+});
