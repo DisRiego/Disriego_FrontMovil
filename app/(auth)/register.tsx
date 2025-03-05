39afcd425252ac2e2933cf6bb6cfd05e205d47e2
Anyela <u20211195450@usco.edu.co>
2025-03-05 02:33:29 -0500

    refactor: agregar SafeAreaView y Header

--- app/(auth)/register.tsx
+++ app/(auth)/register.tsx
@@ -5,15 +5,16 @@ import {
   TouchableOpacity,
   Image,
   StyleSheet,
+  SafeAreaView,
 } from "react-native";
 import { useState } from "react";
 import { useRouter, Stack } from "expo-router";
-import { colors } from "../../config/theme";
-import { typography } from "../../config/typography";
-import Button from "../../components/Button";
-import DropdownPicker from "../../components/Dropdown";
+import { colors } from "@/config/theme";
+import { typography } from "@/config/typography";
+import Button from "@/components/Button";
+import DropdownPicker from "@/components/Dropdown";
 import CustomInput from "@/components/CustomInput";
-import Header from "../../components/Header";
+import Header from "@/components/Header";
 
 export default function RegisterScreen() {
   const router = useRouter();
@@ -24,52 +25,59 @@ export default function RegisterScreen() {
   }
 
   return (
-    <View style={styles.container}>
-      <View style={styles.formContainer}>
-        <Text style={[typography.semibold.big, { color: colors.darkGray }]}>
-          Registrarse
-        </Text>
-        <Text style={[typography.medium.regular, { color: colors.gray }]}>
-          Por favor, proporcione la siguiente información para confirmar su
-          registro en el sistema.
-        </Text>
-        <View style={styles.inputRow}>
-          <View style={{ width: "30%" }}>
-            <DropdownPicker
-              selectedValue={documentType}
-              onValueChange={setDocumentType}
-              options={[
-                { label: "Cedula Ciudadanía", value: "C.C" },
-                { label: "Tarjeta Identidad", value: "T.I" },
-                { label: "Pasaporte", value: "Pasaporte" },
-              ]}
+    <SafeAreaView style={styles.safeArea}>
+      <Header />
+      <View style={styles.container}>
+        <View style={styles.formContainer}>
+          <Text style={[typography.semibold.big, { color: colors.darkGray }]}>
+            Registrarse
+          </Text>
+          <Text style={[typography.medium.regular, { color: colors.gray }]}>
+            Por favor, proporcione la siguiente información para confirmar su
+            registro en el sistema.
+          </Text>
+          <View style={styles.inputRow}>
+            <View style={{ width: "30%" }}>
+              <DropdownPicker
+                selectedValue={documentType}
+                onValueChange={setDocumentType}
+                options={[
+                  { label: "Cedula Ciudadanía", value: "C.C" },
+                  { label: "Tarjeta Identidad", value: "T.I" },
+                  { label: "Pasaporte", value: "Pasaporte" },
+                ]}
+              />
+            </View>
+            <CustomInput
+              placeholder="No. de Identificación *"
+              style={{ width: "65%" }}
             />
           </View>
-          <CustomInput
-            placeholder="No. de Identificación *"
-            style={{ width: "65%" }}
-          />
+
+          <CustomInput placeholder="Fecha de Expedición *" />
         </View>
 
-        <CustomInput placeholder="Fecha de Expedición *" />
-      </View>
+        <View style={styles.footerContainer}>
+          <Button text={"Registrarse"} onPress={handleRegister} />
 
-      <View style={styles.footerContainer}>
-        <Button text={"Registrarse"} onPress={handleRegister} />
-
-        <Text style={[typography.medium.regular, { color: colors.gray }]}>
-          ¿Tienes una cuenta?
-          <Text style={styles.link} onPress={() => router.push("/login")}>
-            {" "}
-            Regístrate aquí
+          <Text style={[typography.medium.regular, { color: colors.gray }]}>
+            ¿Tienes una cuenta?
+            <Text style={styles.link} onPress={() => router.push("/login")}>
+              {" "}
+              Inicia sesión aquí
+            </Text>
           </Text>
-        </Text>
+        </View>
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: colors.base,
+  },
   container: {
     flex: 1,
     alignItems: "center",
