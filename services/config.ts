38b8697e4cb5979a0f4c9299212052d4abde46c5
Anyela <u20211195450@usco.edu.co>
2025-03-15 22:19:27 -0500

    update: cambio en la ruta

--- services/config.ts
+++ services/config.ts
@@ -1,4 +1,4 @@
 export const API_URL = "https://disriego-backend.onrender.com";
 export const API_FRONT = "https://disriego.vercel.app/";
 
-// export const API_URL = "http://192.168.1.54:8002";
+// export const API_URL = "http://ip:port";
