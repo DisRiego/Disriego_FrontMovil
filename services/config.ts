434f9969f409ee7e5e76020681aa592ab0da4a69
Anyela <u20211195450@usco.edu.co>
2025-03-29 13:15:40 -0500

    update: ajuste en API_URL y API_FRONT

--- services/config.ts
+++ services/config.ts
@@ -1,5 +1,5 @@
-// export const API_URL = "http://ip:port";
+export const API_URL = "http://192.168.1.54:8002";
 
-//export const API_URL = ""https://disriego-backend.onrender.com";
+//export const API_URL = "https://disriego-backend.onrender.com";
 
-export const API_FRONT = "https://disriego.vercel.app/";
+export const API_FRONT = "https://disriegos.vercel.app/";
