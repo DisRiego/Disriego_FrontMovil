d017c922423692ba3c853ebd3372bb1b7fe35240
Anyela <u20211195450@usco.edu.co>
2025-04-01 03:14:10 -0500

    update: aclaraciones en config de URLs para desarrollo y producción

--- services/config.ts
+++ services/config.ts
@@ -1,5 +1,12 @@
+// Dirección del backend principal
+// Durante desarrollo, puedes usar tu IP local seguida del puerto correspondiente.
+
 export const API_URL = "http://ip:port";
 
-//export const API_URL = "https://disriego-backend.onrender.com";
+// Dirección del backend de dispositivos IoT
+export const API_URL_IOT = "http://ip:port";
 
-export const API_FRONT = "https://disriegos.vercel.app/";
+// Producción: https://disriego-backend.onrender.com
+
+// Dirección del frontend
+export const API_FRONT = "https://disriegos.vercel.app";
