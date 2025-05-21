b1adff7671ae6f9face343286bf7f596eaa7c56a
Anyela <u20211195450@usco.edu.co>
2025-05-21 14:27:47 -0500

    update: agregan endpoints de facturación y mantenimiento

--- services/config.ts
+++ services/config.ts
@@ -5,11 +5,13 @@
 // export const API_URL = "http://ip:port";           // API general
 // export const API_URL_IOT = "http://ip:port";       // API de IoT
 // export const API_URL_MAINT = "http://ip:port";     // API de mantenimiento
+//export const API_URL_FACT = "http://ip:port"; // API de facturación
 
 // --- PRODUCCIÓN ---
 // Si trabajas conectado a la nube (onrender), utiliza las siguientes URLs:
-//export const API_URL = "https://disriego-backend.onrender.com";        // API general
-//export const API_URL_IOT = "https://backend-iot-2377.onrender.com";    // API de IoT
-//export const API_URL_MAINT = "";                                     // API de mantenimiento
+export const API_URL = "https://disriego-backend.onrender.com"; // API general
+export const API_URL_IOT = "https://backend-iot-2377.onrender.com"; // API de IoT
+export const API_URL_MAINT = "https://backendmaintenance.onrender.com"; // API de mantenimiento
+export const API_URL_FACT = "https://backend-facturation-qhjk.onrender.com"; // API de facturación
 
 export const API_FRONT = "https://disriegos.vercel.app"; // Frontend de producción
